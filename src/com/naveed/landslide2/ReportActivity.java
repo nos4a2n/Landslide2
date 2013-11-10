a7474429d3349826ecc82db7c2479d80e2fcfc54
nos4a2n <naveedfahmad@gmail.com>
2013-11-10 20:42:51 +0800

    Test Version 1.0
    -All major requirements fulfilled
    -Registration module included
    -Login module included
    -Reporting module included
    -View Report module included
    -Google Maps and Places module included

--- src/com/naveed/landslide2/ReportActivity.java
+++ src/com/naveed/landslide2/ReportActivity.java
@@ -1,47 +1,132 @@
 package com.naveed.landslide2;
 
+import java.text.DecimalFormat;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.model.LatLng;
 import com.naveed.landslide2.library.ReportFunctions;
 import com.naveed.landslide2.library.JSONParser;
 
-public class ReportActivity extends Activity{
-	
+public class ReportActivity extends Activity implements
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener,
+		com.google.android.gms.location.LocationListener {
+
 	ReportFunctions reportFunctions;
-	Button viewReportBtn;
-	
+	ImageButton sendReportBtn;
+	EditText extraMessage;
+	String message = "";
+	String uid = "";
+
+	LocationClient mLocationClient;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.report);
-		
-		viewReportBtn = (Button) findViewById(R.id.button1);
-		
-		viewReportBtn.setOnClickListener(new View.OnClickListener() {
-			
+
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationClient.connect();
+
+		sendReportBtn = (ImageButton) findViewById(R.id.btnSendReport);
+		extraMessage = (EditText) findViewById(R.id.textMsg);
+
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			uid = extras.getString("uid");
+		}
+
+		sendReportBtn.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				reportFunctions = new ReportFunctions();
-				JSONObject json = reportFunctions.viewReport("2");
-				try {
-					JSONObject jsonReport = json.getJSONObject("1");
-					Log.d("testing", jsonReport.getString("uid"));
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+				Location lastLoc = mLocationClient.getLastLocation();
+
+				// create LatLng
+				double lat = lastLoc.getLatitude();
+				double lng = lastLoc.getLongitude();
+				DecimalFormat df = new DecimalFormat("###.######");
+				String latitude = df.format(lat).toString();
+				String longitude = df.format(lng).toString();
+				message = extraMessage.getText().toString();
+				ReportFunctions reportFunctions = new ReportFunctions();
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						ReportActivity.this);
+				builder.setMessage("Report has been sent!");
+				builder.setCancelable(false);
+				builder.setNeutralButton("OK",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// TODO Auto-generated method stub
+								ReportActivity.this.finish();
+							}
+						});
+				if (reportFunctions.sendReport(uid, latitude, longitude,
+						message) != null) {
+					AlertDialog alert = builder.create();
+					alert.show();
 				}
-				
+
 			}
 		});
+
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "Connected!", Toast.LENGTH_SHORT).show();
+		LocationRequest request = LocationRequest.create();
+		request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		request.setInterval(30000);
+		request.setFastestInterval(0);
+		mLocationClient.requestLocationUpdates(request, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		// String msg = "Location: " + location.getLatitude() + ", " +
+		// location.getLongitude();
+		// Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 	}
 }
---
+++ src/com/naveed/landslide2/ReportListActivity.java
@@ -0,0 +1,110 @@
+package com.naveed.landslide2;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.naveed.landslide2.library.ReportFunctions;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ReportListActivity extends ListActivity {
+
+	ReportFunctions reportFunctions;
+	ArrayList<String> dataHolder = new ArrayList<String>();
+	String recordText = "";
+	String details = "";
+	String uid = "";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.report_list);
+		reportFunctions = new ReportFunctions();
+
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+			uid = extras.getString("uid");
+		}
+
+		JSONObject data = reportFunctions.viewReport(uid);
+		try {
+			String success = data.getString("success");
+			if (Integer.parseInt(success) == 1) {
+				for (int i = 0; i < data.length() - 3; i++) {
+					recordText = "";
+					String x = "" + i;
+					try {
+						JSONObject record = data.getJSONObject(x);
+						recordText = "Report " + (i + 1) + " = "
+								+ record.getString("time").toString();
+						dataHolder.add(recordText);
+						Log.d("tester", recordText);
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				setListAdapter(new ArrayAdapter<String>(this,
+						android.R.layout.simple_list_item_1, dataHolder));
+			} else if (data.toString() == "n") {
+				Toast.makeText(this, "No records found!", Toast.LENGTH_LONG)
+						.show();
+			}
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			Toast.makeText(this, "No records found!", Toast.LENGTH_LONG).show();
+			e1.printStackTrace();
+
+		}
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		JSONObject data = reportFunctions.viewReport(uid);
+		String x = "" + position;
+		try {
+			JSONObject record = data.getJSONObject(x);
+			recordText = "Report Details:\nReport ID = "
+					+ record.getString("rid").toString() + "\nDate and Time = "
+					+ record.getString("time").toString() + "\nLatitude = "
+					+ record.getString("lat").toString() + "\nLongitude = "
+					+ record.getString("lng").toString();
+			Log.d("tester", recordText);
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(
+					ReportListActivity.this);
+			builder.setMessage(recordText);
+			builder.setCancelable(false);
+			builder.setNeutralButton("OK",
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							dialog.cancel();
+						}
+					});
+
+			AlertDialog alert = builder.create();
+			alert.show();
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
